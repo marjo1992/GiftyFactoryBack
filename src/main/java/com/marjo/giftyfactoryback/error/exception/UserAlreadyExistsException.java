4c35ee810dc5095d2e7815ecc00a2f9684846554
marjo1992 <verelmarjolaine@hotmail.fr>
2024-01-22 17:41:55 +0100

    fix error message missing in UserAlreadyExistsException

--- src/main/java/com/marjo/giftyfactoryback/error/exception/UserAlreadyExistsException.java
+++ src/main/java/com/marjo/giftyfactoryback/error/exception/UserAlreadyExistsException.java
@@ -3,7 +3,8 @@ package com.marjo.giftyfactoryback.error.exception;
 
 public class UserAlreadyExistsException extends RuntimeException{
 
-    public UserAlreadyExistsException(String string) {
+    public UserAlreadyExistsException(String message) {
+        super(message);
     }
 
 }
